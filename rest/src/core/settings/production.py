10073ed7e3d2f2acc467e3d9bf3b0c5258301410
DipeshAmazatic <dipesh.k@amazatic.com>
2022-03-22 14:43:43 +0530

    update production files

--- rest/src/core/settings/production.py
+++ rest/src/core/settings/production.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mtv&b90maj&ild^zub319cvw@p#yhlg0+2f74t5dh+^iomu1x8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if (os.environ.get("DEBUG", "true").lower() == "true") else False
@@ -23,16 +23,10 @@ ALLOWED_HOSTS = ["*"]
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('NAME'),
-        'HOST': os.environ.get('HOST'),
-        'PORT': os.environ.get('PORT'),
-        'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('PASSWORD'),
-    }
-}
+DEFAULT_CONNECTION = dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DEFAULT_CONNECTION.update({"CONN_MAX_AGE": 600})
+DATABASES = {"default": DEFAULT_CONNECTION}
+print('connected through database...')
 
 
 ## send email using smtp ##
