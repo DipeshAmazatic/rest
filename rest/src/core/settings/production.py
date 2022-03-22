02a00000497846329b1a53578679066c3d872f05
DipeshAmazatic <dipesh.k@amazatic.com>
2022-03-22 16:17:46 +0530

    modify production file

--- rest/src/core/settings/production.py
+++ rest/src/core/settings/production.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mtv&b90maj&ild^zub319cvw@p#yhlg0+2f74t5dh+^iomu1x8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #if (os.environ.get("DEBUG", "true").lower() == "true") else False
